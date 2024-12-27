bea16fd547c125aee416de236e75779925756a8b
Iebin Lian <122533915+iblian@users.noreply.github.com>
2024-12-27 23:17:39 +0800

    FL_corr.R

--- FL_corr.R
+++ FL_corr.R
@@ -1,23 +1,27 @@
-#input X
-#install.packages : readxl
-#no missing value is allowed
-#Please refer to the excel file for the data format.
-#@excemple
-XA<-c(1,2,3,4,5,6,1,2,3,4,5,6);YA<-c(2,3,4,5,6,7,0,1,2,3,4,5);A<-data.frame(XA,YA)
-XB<-c(7,8,9,10,11,12,7,8,9,10);YB<-c(8,9,10,11,12,9,5,6,7,8);B<-data.frame(XB,YB)
-XC<-c(1,2,3,1,2,3,4,5,6,7,8,11);YC<-c(5,7,9,11,2,1,4,5,6,8,10,13);C<-data.frame(XC,YC)
-colnames(A) <- c("X", "Y");colnames(B) <- c("X", "Y");colnames(C) <- c("X", "Y")
-r_i <- c(cor(A)['X','Y'],cor(B)['X','Y'],cor(C)['X','Y']) # corr
-Xmean_i <- c(mean(A$X),mean(B$X),mean(C$X)) # X Mean
-Ymean_i <- c(mean(A$Y),mean(B$Y),mean(C$Y)) # Y Mean
-varX_i <- c(var(A$X),var(B$X),var(C$X))  # X Varience
-varY_i <- c(var(A$Y),var(B$Y),var(C$Y))  # Y Varience
-n_i <- c(nrow(A),nrow(B),nrow(C)) # Sample size
-data<-cbind(r_i,Xmean_i,Ymean_i,varX_i ,varY_i,n_i)
-write.csv(data, file = paste0("C:/Users/Users/Desktop/test.csv"), row.names = FALSE)
-data<-read.csv("C:/Users/Users/Desktop/test.csv",header = T)
+#' input mean_x, mean_y, Var_x, Var_y, correlation (x,y)
+#' install.packages : readxl
+#' no missing value is allowed
+#' Please refer to the excel file for the data format.
+#' @excemples
+#' \dontrun{
+#'  #simulation data
+#'   XA<-c(1,2,3,4,5,6,1,2,3,4,5,6);YA<-c(2,3,4,5,6,7,0,1,2,3,4,5);A<-data.frame(XA,YA)
+#'   XB<-c(7,8,9,10,11,12,7,8,9,10);YB<-c(8,9,10,11,12,9,5,6,7,8);B<-data.frame(XB,YB)
+#'   XC<-c(1,2,3,1,2,3,4,5,6,7,8,11);YC<-c(5,7,9,11,2,1,4,5,6,8,10,13);C<-data.frame(XC,YC)
+#'   colnames(A) <- c("X", "Y");colnames(B) <- c("X", "Y");colnames(C) <- c("X", "Y")
+#'   r_i <- c(cor(A)['X','Y'],cor(B)['X','Y'],cor(C)['X','Y']) # corr
+#'   Xmean_i <- c(mean(A$X),mean(B$X),mean(C$X)) # X Mean
+#'   Ymean_i <- c(mean(A$Y),mean(B$Y),mean(C$Y)) # Y Mean
+#'   varX_i <- c(var(A$X),var(B$X),var(C$X))  # X Varience
+#'   varY_i <- c(var(A$Y),var(B$Y),var(C$Y))  # Y Varience
+#'   n_i <- c(nrow(A),nrow(B),nrow(C)) # Sample size
+#'   data<-cbind(r_i,Xmean_i,Ymean_i,varX_i ,varY_i,n_i)
+#'   write.csv(data, file = paste0("C:/Users/Users/Desktop/test.csv"), row.names = FALSE)
+#'  }
 
-#data<-read.csv("your data path/test.csv",header = T)
+#'install.packages : readxl
+ 
+data<-read.csv("your data path/test.csv",header = T)
 FL_corr <- function(data) {
   # read data
   data=data
@@ -63,3 +67,4 @@ FL_corr <- function(data) {
   cat("point estimate =", beta_hat_total, "\n")
   cat("standard deviation =", se_beta_total, "\n")
 }
+
